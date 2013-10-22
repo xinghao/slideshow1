d87db7d4b47d0dcc2c999e30643cb2ac16a8b10d
xinghao <Xinghao@airarena.net>
2013-10-23 10:20:43 +1100

    fix popup form not showing problem

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,9 +1,17 @@
+/*
 jQuery(document).ready(function($){
-  $( "div.button" )
+  $( "div.button a" )
     .click(function() {    
+    alert("alo");
     var src = $(this).attr("data_target");
-    $(this).hide();
+    $(this).parent().hide();
     $("div#" + src).show();
     });
 });   
+*/
+function popupBox(srcid,targetid)
+{  
+    $("a#" + srcid).parent().hide();
+    $("div#" + targetid).show();
+}
                        
--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,6 +1,6 @@
 module StaticHelper
   def random_background_images
-    images=["background-1b.jpg","background-2b.jpg","background-3b.jpg","background-4b.jpg","background-5b.jpg","background-6b.jpg"]
+    images=["background-1b.jpg","background-2b.jpg","background-3b.jpg","background-4b.jpg","background-5b.jpg"]
     index = rand(0..(images.size - 1))
     return images[index]  
   end
