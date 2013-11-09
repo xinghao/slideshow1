67f7dca4d5b839076c05821af8844ed9f522313d
xinghao <Xinghao@airarena.net>
2013-11-10 00:26:11 +1100

    fix by annar

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,15 +1,15 @@
 class NoticeMailer < ActionMailer::Base
-  default to: "xinghao.yu@airarena.net,jaronchester@gmail.com", from: "no-reply@tuneitradio.com"
+  default to: "xinghao.yu@airarena.net,jaronchester@gmail.com", from: "admin@travelyenta.com"
   
   
   def booking_email(booking)
     @booking = booking
-    mail(:subject => "New booking [#{@booking.email}] for yenta")
+    mail(:subject => "New booking [#{@booking.email}] for yenta", :to => "bookings@travelyenta.com,xinghao.yu@airarena.net,jaronchester@gmail.com")
   end
   
   def enquire_email(enquire)
     @enquire = enquire
-    mail(:subject => "New enquire [#{@enquire.email}] for yenta")
+    mail(:subject => "New enquire [#{@enquire.email}] for yenta", :to => "bookings@travelyenta.com,xinghao.yu@airarena.net,jaronchester@gmail.com")
   end
   
 
